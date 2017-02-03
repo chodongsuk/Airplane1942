e3f1127c2389b274ee61eb2e433acc1648c5ac48
조동석 <lab2734@gmail.com>
2017-02-03 18:55:53 +0900

    Initial commit

---
+++ app/src/main/java/com/kr/airplane1942/WebApplication.java
@@ -0,0 +1,16 @@
+package com.kr.airplane1942;
+
+import android.support.multidex.MultiDexApplication;
+
+
+public class WebApplication extends MultiDexApplication {
+	/**
+	 * 이미지 로더, 이미지 캐시, 요청 큐를 초기화한다.
+	 */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+
+	}
+}
